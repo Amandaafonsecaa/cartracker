47047ede669c44aaf842af443bc1bbb7527402c0
Amandaafonsecaa <amandafonsecarod09@gmail.com>
2026-07-01 21:59:33 -0300

    feat: add layout component wrapping header and main content

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Header } from './components/layout/header/Header';
+import { Layout } from './components/layout/Layout';
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />        {/* ← fora do Routes, aparece em todas as páginas */}
-      <main>
+      <Layout>
         <Routes>
           <Route path="/" element={<div>Dashboard</div>} />
           <Route path="/refueling" element={<div>Abastecimento</div>} />
@@ -13,9 +12,9 @@ function App() {
           <Route path="/history" element={<div>Histórico</div>} />
           <Route path="/vehicles" element={<div>Veículos</div>} />
         </Routes>
-      </main>
+      </Layout>
     </BrowserRouter>
-  );
+  )
 }
 
 export default App;
